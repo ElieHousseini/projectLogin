224e57e0d050e5d461d1ad2d6ede73d9c800b8dd
ElieHousseini <=eliashousseini@gmail.com>
2021-08-23 23:14:39 +0300

    temporary fix, because there is a dependency problem

--- SetupHelper.js
+++ SetupHelper.js
@@ -1,6 +1,6 @@
-// import { AsyncStorage } from 'react-native';
+import { AsyncStorage } from 'react-native';
 
-import AsyncStorage from '@react-native-community/async-storage'
+// import AsyncStorage from '@react-native-community/async-storage'
 const SetupHelper = {
     getItemAsyncStorage: async (item) => {
         try {
